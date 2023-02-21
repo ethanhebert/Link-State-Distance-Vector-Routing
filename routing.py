23e9d41ef07135fb655e10e98fd4ddef12402283
Lucas6658 <80854527+Lucas6658@users.noreply.github.com>
2023-02-21 17:13:50 -0600

    Changed a thing or two and added comments
    Main change was that now I have made allValues an outside variable to keep track of all the values of all the nodes, but without the node's letter. I guess this could also have been done with topology[i][1:] but I did it my own way since I was already going through all the values of all the nodes to find who is a neighbor and who is not anyway.
    
    So now I have a list of all the nodes without an empty string before it, a list of lists of all their values corresponding to their indexes, and a list of lists of all their neighbors. I think with this, we can create a function to calculate Bellman-Ford, because I cannot wrap my head around a way to do it without recursion.
    
    For instance, most of the code is inside a for loop, the first iteration is the first node, let's say node U. Now all I have all the values and neighbors for U, but I cannot calculate any distance vectors to a node that is not a neighbor of U because I only know the neighbors of U at the end of this for loop.
    Hence why I think we might need a function after getting everything so that I know everything already and I just need to grab it. So I would start u to u = 0, u to v = 7, u to w = 3, u to x = 5, then u to y = ??? well, I'd have to call the function again for v, w, and x and see which one of them is neighbors with y or keep calling the function until a neighbor of Y is found, which at the end would return the cost found.
    Still a lot to wrap my head around, and how would I even begin to keep track of the cost if we get a 30x30 topology?

--- routing.py
+++ routing.py
@@ -97,15 +97,28 @@ def ls(topology,src):
 
 # distance vector routing with Bellman-Ford equation
 def dv(topology):
-    str1 = ""
+    # Create a list of all the nodes, without an empty string before it
+    allNodes = topology[0][1:] 
+    
+    # Create a list that will hold only the values to each node, without an empty string before it
+    # This will be a list of lists
+    allValues = [0 for q in range(len(allNodes))] 
 
-    allNodes = topology[0][1:]
-
+    # Create a list to hold all of the nodes' neighbors
+    # This will be a list of lists
     nodesNeighbors = ["" for c in range(len(allNodes))]
 
-    for j in range(1, len(allNodes) + 1):
-        allValues = topology[j][1:]
-        neighborsNum = len(allValues) - allValues.count("9999") - 1
+    # For loop that will iterate the number of nodes there are
+    # Starts at 1 becuase it makes it a little bit easier here in the beginning since
+    # there is the node it refers to on each row
+    # It would be tricky to do j + 1 instead since it could land on an Index out of Range error
+    for j in range(1, len(allNodes) + 1): # 6 iterations for topology-1
+
+        # Create a list of the node's values to each of its neighbors
+        values = topology[j][1:]
+
+        # Variable that counts how many neighbors the node has
+        neighborsNum = len(values) - values.count("9999") - 1
 
         # So now I know how many neighbors a node has, but I do not know who they are
         # I want a list with an X number of empty strings; X is the number of neighbors I know I have
@@ -113,29 +126,69 @@ def dv(topology):
 
         neighbors = ["" for z in range(neighborsNum)]
 
-        # then go through allValues list and get the index of the values that arent 0 or 9999
-        # Add allNodes[allValues.index(number that is not 0 or 9999)] to neighbors list.
-        # So for u: v, w, x values are not 0 or 9999, so get their indexes of 1, 2, and 3
+        # then go through values list and get the index of the values that arent 0 or 9999
+        # Add allNodes[index of number that is not 0 or 9999] to neighbors list.
+        # Then add that neighbors list to the nodesNeighbors list of lists.
+        # So for topology-1, u: v, w, x values are not 0 or 9999, so get their indexes of 1, 2, and 3
         # and add to neighbors[0] = allNodes[1], 
         # neighbors[1] = allNodes [2], neighbors[2] = allNodes[3]
-        # At the end, nodeNeighbors should be: ["v", "w", "x"] for node u
+        # At the end, neighbors for node u will be: ["v", "w", "x"]
 
+        # k variable will keep track of where to put the neighbor node in neighbor list
         k = 0
+
+        # valIndex will keep track of where the neighbor node is to give it to allNodes to put it in neighbor list
         valIndex = 0
+
+        # Create a list of all the values for that node, so that it can be added to allValues list of lists
+        myValues = [0 for w in range(len(allNodes))]
         
-        for val in allValues:    
+        # val should be an integer, but the way it is stored in the list, it is a string
+        # so for the if statements, val needs to made an integer for the condition statements
+        for val in values:
+
+            # Add the value to the list myValues to keep all this node neighbor's values
+            myValues[valIndex] = int(val)
+
+            # If a value is not 0 and 9999, it is a neighbor, so add it to the neighbor list and increment k
             if (int(val) != 0 and int(val) != 9999):
                 neighbors[k] = allNodes[valIndex]
                 k = k + 1
-            if (k == neighborsNum):
-                break
+
+            # Since val can be any number, this variable is needed to keep count of the loop's iterations
             valIndex = valIndex + 1
+
+        # Add the node neighbor's values to the list, making it a list of lists
+        # So allValues[0] is a list of all the neighbors' values of the first node
+        # And int(allValues[0][0]) is an integer of the first value of the first node, which should be 0
+        # For topology-1, this is a list that holds 6 lists that each hold 6 values
+        # Make sure to make them integers before using them for if statements and such
+        allValues[j - 1] = myValues
+
+        # Add the node's neighbors to this list that will keep them, making it a list of lists
+        # So nodesNeighbors[0] is a list of all the neighbors of the first node
+        # And nodesNeighbors[0][0] is a string of the first neighbor of the first node.
         nodesNeighbors[j - 1] = neighbors
+
+        # now what I want to do is go through each singular node and its neighbors
+        # for u, it has 3 neighbors. Then calculate DV for u to u, u to v, u to w, u to x
+        # so I probably want 2 nested for loops, or a funciton, the outside go through the node
+        # the inside through its neighbors. A function might be needed since recursion might have to be used.
+        # for instance, from u to z, if u goes to w, w then has to go to x or y which are neighbors of z
+        # I can use the outside loop from j = 1 to len(allNodes) + 1 as the outside loop, but I
+        # would not have access to the other nodes' neighbors.
+
+        # need help making a function for this, I am struggling putting anything into code at this point.       
         
+            
+
+    str1 = ""
+    # print statement at the end
+    # will need to add another {} for the DV found
     for node in range(len(allNodes)):
         str1 += "Distance vector for node {}:\n".format(allNodes[node])
         
-    print(str1)
+    print(str1) 
 
 ### MAIN ###
 # check for proper input args
